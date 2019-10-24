9859c5db2d7814880e16d2856418418c8b65dac2
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 16:29:27 -0700

    word counts completed

--- objects.js
+++ objects.js
@@ -4,10 +4,22 @@
 /** 1. countWords */
 function countWords(phrase) {
 	// Replace this with your code
+  const word_counts = {};
+
+  for (const word  of phrase.split(" ")) {
+
+    if (word in word_counts) {
+      word_counts[word] += 1;
+    } else
+      word_counts[word] = 1;
+  }
+
+  return word_counts;
+
 }
 
 
-/** 2. getMelonsAtPrice */
-function getMelonsAtPrice(price) {
-	// Replace this with your code
-}
+// /** 2. getMelonsAtPrice */
+// function getMelonsAtPrice(price) {
+// 	// Replace this with your code
+// }

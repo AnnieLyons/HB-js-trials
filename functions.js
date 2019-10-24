7272a6c14335e0f581315eb7f60da2535278f715
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 16:18:03 -0700

    completed functions-calculateTotal

--- functions.js
+++ functions.js
@@ -1,38 +1,37 @@
 "use strict";
 
 
-/** 1. isHometown */
+// 1. isHometown
 function isHometown(town) {
 
   return town === 'San Francisco';
 }
 
-// ** 2. getFullName */
-function getFullName(first_name, last_name){
-
+// 2. getFullName 
+function getFullName(first_name, last_name) {
  return `${first_name} ${last_name}`;
 }
 
 
-/** 3. calculateTotal */
-function calculateTotal(base_price, state, tax=0.05){
+// 3. calculateTotal
+function calculateTotal(base_price, state, tax = 0.05) {
+  const subtotal = base_price * (1 + tax);
 
-// Python Code Below!
-// subtotal = base_price * (1 + tax)
+  let fee = 0;
 
-//     fee = 0
-//     if state == 'CA':
-//         fee = 0.03 * subtotal
-//     elif state == 'PA':
-//         fee = 2
-//     elif state == 'MA':
-//         if base_price <= 100:
-//             fee = 1
-//         else:
-//             fee = 3
+  if (state === 'CA') {
+    fee = 0.03 * subtotal;
+  } else if (state === 'PA') {
+    fee = 2;
+  } else if (state === 'MA') {
+    if (base_price <= 100) {
+      fee = 1;
+    } else {
+      fee = 3;
+    }
+  }
 
-//     return subtotal + fee
-
-
+  return subtotal + fee;
 }
+
 

7e4ba240d6f66ba77a1196d1390b10b5706ac009
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 16:35:39 -0700

    completed functions - getMelonsAtPrice

--- objects.js
+++ objects.js
@@ -19,7 +19,17 @@ function countWords(phrase) {
 }
 
 
-// /** 2. getMelonsAtPrice */
-// function getMelonsAtPrice(price) {
-// 	// Replace this with your code
-// }
+// 2. getMelonsAtPrice 
+function getMelonsAtPrice(price) {
+	// Replace this with your code
+  const melon_prices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  }
+
+  if (price in melon_prices) {
+    return melon_prices[price].sort();
+  }
+}
